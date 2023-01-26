b54f3b00a06f03eb433b84eb0f4b91fd8cd27af5
loreen <loreenmuusha@gmail.com>
2023-01-26 11:40:01 +0200

    branching from master

--- reactjs-form-input-tracking/src/components/Header.jsx
+++ reactjs-form-input-tracking/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <h1 style={{marginBottom: '5rem'}}>Reactjs Form Input Tracking</h1>
+    <h1 style={{marginBottom: '6rem'}}>Reactjs Form Input Tracking</h1>
   )
 }
 
